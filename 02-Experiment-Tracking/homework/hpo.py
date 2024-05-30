d231df8e3a37515f3f351d977eb6f0a3b559e5eb
Nembot Jules <nembotjulesgithub@gmail.com>
2024-05-30 19:30:55 +0100

    Hyperparameter Optimization

---
+++ 02-Experiment-Tracking/homework/hpo.py
@@ -0,0 +1,57 @@
+import os
+import pickle
+import mlflow
+import numpy as np
+from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
+from hyperopt.pyll import scope
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("random-forest-hyperopt")
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+def run_optimization(data_path: str, num_trials: int):
+
+    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+    def objective(params):
+
+        with mlflow.start_run():
+            mlflow.set_tag("model", "RandomForestRegressor")
+            mlflow.log_params(params)
+
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            rmse = mean_squared_error(y_val, y_pred, squared=False)
+            mlflow.log_metric("rmse", rmse)
+
+        return {'loss': rmse, 'status': STATUS_OK}
+
+    search_space = {
+        'max_depth': scope.int(hp.quniform('max_depth', 1, 20, 1)),
+        'n_estimators': scope.int(hp.quniform('n_estimators', 10, 50, 1)),
+        'min_samples_split': scope.int(hp.quniform('min_samples_split', 2, 10, 1)),
+        'min_samples_leaf': scope.int(hp.quniform('min_samples_leaf', 1, 4, 1)),
+        'random_state': 42
+    }
+
+    rstate = np.random.default_rng(42)  # for reproducible results
+    fmin(
+        fn=objective,
+        space=search_space,
+        algo=tpe.suggest,
+        max_evals=num_trials,
+        trials=Trials(),
+        rstate=rstate
+    )
+
+data_path = "/home/maxtheking/mlopszoomcamp/02-Experiment-Tracking/homework/output"
+run_optimization(data_path, num_trials = 15)
